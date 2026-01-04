4556beeedbc6f114627cfdfe72bebce1ce64f8c0
Dina Shanjida <140268815+Dina-Shanjida@users.noreply.github.com>
2026-01-04 17:42:22 +0000

    aging implementation by tithy

--- src/scheduler.c
+++ src/scheduler.c
@@ -13,31 +13,41 @@ void scheduler_init(int quantum) {
 void scheduler_tick() {
     tickCounter++;
 
+    for (int i = 0; i < MAX_PROC; i++) {
+        if (proc_table[i].state == READY) {
+            proc_table[i].age++;
+        }
+    }
+
     if (tickCounter < timeSlice)
         return;
 
     tickCounter = 0;
 
     int selectedProcess = -1;
+    int highestAge = -1;
 
     for (int i = 0; i < MAX_PROC; i++) {
-        if (proc_table[i].state == READY) {
+        if (proc_table[i].state == READY && proc_table[i].age > highestAge) {
+
+            highestAge = proc_table[i].age;
             selectedProcess = i;
-            break;
+        
         }
     }
 
     if (selectedProcess < 0)
         return;
 
-    if (curr_pid >= 0 &&
-        proc_table[curr_pid].state == RUNNING) {
+    if (curr_pid >= 0 && proc_table[curr_pid].state == RUNNING) {
 
         proc_table[curr_pid].state = READY;
     }
 
     curr_pid = selectedProcess;
+    
     proc_table[selectedProcess].state = RUNNING;
+    proc_table[selectedProcess].age = 0;
 
     proc_table[selectedProcess].entry();
 }
